bc3ad8e60ecc26a14ca975494b4684cbc37c8e61
gizmore <gizmore@wechall.net>
2026-09-18 04:07:40 +0200

    'more korean'

---
+++ lang/spider_ko.php
@@ -0,0 +1,12 @@
+<?php
+namespace GDO\DogIRCSpider\lang;
+
+return [
+	'dog_help_crawl' => '채널에 대한 IRC 네트워크를 크롤링하고 참여하세요.',
+	'err_dog_already_crawling' => '%s은(는) 이미 크롤링 중입니다.',
+	'msg_dog_crawl_inited' => '%s 크롤링을 시작했습니다.',
+	'msg_crawler_joined' => '안녕하세요 %s님. 저는 친절한 챗봇 %s입니다. $help를 통해 더 자세히 알아볼 수 있습니다.
+나를 걷어차거나 $part를 사용하여 나를 쫓아낼 수 있습니다. 소란을 일으킨 데 대해 %s을(를) 비난하세요.',
+	'msg_crawler_cleared' => '이제 네트워크를 다시 크롤링할 수 있습니다.',
+	'msg_dog_crawl_done' => '크롤러가 완료되었습니다.',
+];
